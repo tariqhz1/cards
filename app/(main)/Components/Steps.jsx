beee770ebc12812e311c0beddf67cc7fd9dc3e75
umer-shahidd <umer45769@gmail.com>
2024-06-15 14:34:24 +0500

    [Hot Fix]

--- app/(main)/Components/Steps.jsx
+++ app/(main)/Components/Steps.jsx
@@ -80,8 +80,8 @@ export default function Steps() {
               }
             }}
           >
-            {cards.map((card) => (
-              <div className="text-base leading-7 mt-14">
+            {cards.map((card, index) => (
+              <div className="text-base leading-7 mt-14" key={index}>
                 <h3 className="font-semibold text-white">{card.name}</h3>
                 <p className="mt-2 text-gray-300 pr-8">{card.description}</p>
               </div>
@@ -89,24 +89,6 @@ export default function Steps() {
           </Carousel>
         </div>
       </div>
-
-      {/* <div className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-6 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-8">
-          {cards.map((card) => (
-            <div
-              key={card.name}
-              className="flex gap-x-4 rounded-xl bg-white/5 p-6 ring-1 ring-inset ring-white/10"
-            >
-              <card.icon
-                className="h-7 w-5 hidden md:flex-none text-indigo-400 "
-                aria-hidden="true"
-              />
-              <div className="text-base leading-7">
-                <h3 className="font-semibold text-white">{card.name}</h3>
-                <p className="mt-2 text-gray-300">{card.description}</p>
-              </div>
-            </div>
-          ))}
-        </div> */}
     </div>
   );
 }
